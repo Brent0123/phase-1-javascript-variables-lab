ba73a2d7987a131a671539ea2935a1f304279335
Cheruiyot Brent Langat <cheruiyot.langat@student.moringaschool.com>
2024-12-03 16:43:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Define the company name using const
+const companyName = 'Scuber';
+
+// Define the most profitable neighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define the company CEO using let
+let companyCeo = 'Susan Smith';
